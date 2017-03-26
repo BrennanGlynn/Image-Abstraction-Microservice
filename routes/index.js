c3da09fe38403cf62b9929f366a992c567f3c5c9
Brennan Glynn <BrennanGlynn@u.boisestate.edu>
2017-03-26 14:12:26 -0600

    Changed title to Image-Abstraction

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	res.render('index', {
-		title: 'Instagram-Image-Abstraction'
+		title: 'Image-Abstraction'
 	});
 });
 

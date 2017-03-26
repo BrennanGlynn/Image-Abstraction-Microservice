d5eff3ac2689e5b50c316c9bb45928903ef18ef1
Brennan Glynn <BrennanGlynn@u.boisestate.edu>
2017-03-26 13:41:14 -0600

    /search/query/offset

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request-promise');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', {
+		title: 'Instagram-Image-Abstraction'
+	});
+});
+
+router.get('/search/:query/:offset', function (req, res) {
+	var options = {
+		uri: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search',
+		qs: {
+			q: req.params.query,
+			count: 10,
+			offset: req.params.offset * 10
+		},
+		headers: {
+			'Ocp-Apim-Subscription-Key': 'ce079cfbecfa4501bf339e6666a40fdd'
+		},
+		json: true
+	};
+
+	request(options)
+	.then(function (response) {
+		console.log(response);
+		var results = [];
+
+		for (image in response.value) {
+			results.push({
+				"url": response.value[image].contentUrl,
+				"snippet": response.value[image].name,
+				"thumbnail": response.value[image].thumbnailUrl,
+				"hostUrl": response.value[image].hostPageUrl
+			});
+		}
+
+		res.send(results);
+	})
+	.catch(function (err) {
+		console.log(err);
+		res.send(err.body)
+	});
 });
 
 module.exports = router;
